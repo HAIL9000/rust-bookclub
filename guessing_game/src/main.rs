de40fc504c022b5c0aab9afb27354a26fba17831
Hailee Kenney <hailee.kenney@gmail.com>
2021-12-16 22:08:57 -0800

    chapter 2: complete guessing game
    finish up the guessing game program taking advantage
    of loop and match functionality. also add the rand
    crate to our dependencies so we can use it in the
    program.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,38 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        // variables in rust are immutable by default, adding
+        // mut makes them mutable
+        let mut guess = String::new();
 
-    println!("You guess: {}", guess)
+        // & indicates a reference which is also immutable by default,
+        // so to pass a mutable reference it must be &mut
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => { 
+                println!("you win!");
+                break;
+            } 
+        }
+    }
 }
